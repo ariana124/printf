39713a52d6f58a8fb1f4e493b589c53bb5acc04c
ariana124 <ariana.bibiano@gmail.com>
2019-10-28 03:29:00 +0000

    prints character

---
+++ printchar.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <unistd.h>
+
+/**
+ * print_c - function that prints a character
+ * @c: character to be printed
+ * Return: character
+ */
+int print_c(char c)
+{
+        return (write(1, &c, 1));
+
+}
