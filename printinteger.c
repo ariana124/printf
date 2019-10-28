3dbe3891978872b19b3e974ff6e682c84b5675f1
ariana124 <ariana.bibiano@gmail.com>
2019-10-28 03:30:37 +0000

    prints integer

---
+++ printinteger.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_i - prints an integer
+ * @n: integer to be printed
+ * Return: printed number
+ */
+int print_i(int n)
+{
+        if (n < 0)
+        {
+                print_c('-');
+                n = -n;
+        }
+        if (n == 0)
+                print_c('0');
+        if (n / 10)
+                print_i(n / 10);
+        print_c(n % 10 + '0');
+
+        return (n);
+}
