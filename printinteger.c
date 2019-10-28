b2d168cbbdadd414966c2b783a3cea43b4ca0741
ariana124 <ariana.bibiano@gmail.com>
2019-10-28 03:50:17 +0000

    fixed spacing issues

--- printinteger.c
+++ printinteger.c
@@ -7,16 +7,18 @@
  */
 int print_i(int n)
 {
-        if (n < 0)
-        {
-                print_c('-');
-                n = -n;
-        }
-        if (n == 0)
-                print_c('0');
-        if (n / 10)
-                print_i(n / 10);
-        print_c(n % 10 + '0');
+	if (n < 0)
+	{
+		print_c('-');
+		n = -n;
+	}
 
-        return (n);
+	if (n == 0)
+		print_c('0');
+
+	if (n / 10)
+		print_i(n / 10);
+	print_c(n % 10 + '0');
+
+	return (n);
 }
