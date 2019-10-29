5322d9f5c3eeafa0c2fac6621a6bda3e4efc98a5
ariana124 <ariana.bibiano@gmail.com>
2019-10-29 03:18:33 +0000

    added recursion function

--- printinteger.c
+++ printinteger.c
@@ -7,18 +7,32 @@
  */
 int print_i(int n)
 {
+	int count = 0;
+
 	if (n < 0)
 	{
 		print_c('-');
 		n = -n;
+		count = count + 1;
 	}
 
-	if (n == 0)
-		print_c('0');
+	count = count + print_irc(n);
+
+	return (count);
+}
+
+/**
+ * print_irc - prints the integer recursively
+ * @n: integer
+ * Return: the byte count of the integer
+ */
+int print_irc(int n)
+{
+	int i = 1;
 
 	if (n / 10)
-		print_i(n / 10);
+		i = print_irc(n / 10) + 1;
 	print_c(n % 10 + '0');
 
-	return (n);
+	return (i);
 }
