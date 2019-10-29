21ec1187f514ba82bbb11470f3c27cde855a7d43
ariana124 <ariana.bibiano@gmail.com>
2019-10-29 19:31:31 +0000

    added unsigned integer

--- printdecimal.c
+++ printdecimal.c
@@ -7,16 +7,20 @@
  */
 int print_d(int n)
 {
+	unsigned int num;
+
 	int count = 0;
 
 	if (n < 0)
 	{
 		print_c('-');
-		n = -n;
+		num = -n;
 		count = count + 1;
 	}
+	else
+		num = n;
 
-	count = count + print_drc(n);
+	count = count + print_drc(num);
 
 	return (count);
 }
@@ -26,12 +30,12 @@ int print_d(int n)
  * @n: integer
  * Return: the byte count of the integer
  */
-int print_drc(int n)
+int print_drc(unsigned int n)
 {
 	int i = 1;
 
 	if (n / 10)
-		i = print_irc(n / 10) + 1;
+		i = print_drc(n / 10) + 1;
 	print_c(n % 10 + '0');
 
 	return (i);
